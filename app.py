d8e6fa94b916663ba2e78c12cce424c34f27a401
Victor Li <victor@victorli.co>
2026-06-15 20:09:08 -0700

    Minor custom changes

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+@app.route("/hello")
+def hello_world():
+    return render_template("index.html")
