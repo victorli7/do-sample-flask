f28b2de0aaf875f6b4a753eb05af411e2887e3d5
Victor Li <victor@victorli.co>
2026-06-15 20:49:41 -0700

    Remote duplicate rotute

--- app.py
+++ app.py
@@ -7,7 +7,3 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
-
-@app.route("/hello")
-def hello_world():
-    return render_template("index.html")
